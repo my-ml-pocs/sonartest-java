537aa696c69a9a129b85753da78891a68b3e5a26
softengrahmed <softengrahmed@gmail.com>
2025-04-07 19:04:45 -0400

    Update src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java

--- src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
+++ src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
@@ -4,7 +4,8 @@ import java.util.Set;
 
 public class CartesianProduct {
 
-    public static <T> String calculate() {
+    public static <T> String calculate(Set<T> setOfSets) {
         return "";
     }
+// Fixed sonar issue: T is not used in the method.
 }
