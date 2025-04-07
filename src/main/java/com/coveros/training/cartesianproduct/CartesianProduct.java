e308127a003a8335152cd66b30adb753eb316f45
softengrahmed <softengrahmed@gmail.com>
2025-04-07 13:23:23 -0400

    Update src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java

--- src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
+++ src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class CartesianProduct {
 
-    public static <T> String calculate(final Set<T> setOfSets) {
+    public static <T> String calculate() {
         return "";
     }
 }
