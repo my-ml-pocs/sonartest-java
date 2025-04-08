3624c4c1e19818025ddfae45e7f9a7ffdc33f05e
softengrahmed <softengrahmed@gmail.com>
2025-04-07 21:57:50 -0400

    Update src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java

--- src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
+++ src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
@@ -3,6 +3,9 @@ package com.coveros.training.cartesianproduct;
 
 
 public class CartesianProduct {
+    private CartesianProduct() {
+        // private constructor to hide the implicit public one
+    }
 
     public static <T> String calculate() {
         return "";
