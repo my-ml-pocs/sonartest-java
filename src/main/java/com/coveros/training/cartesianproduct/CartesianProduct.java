2c12dfd01a8cad32abcfbc0cf78d8896c4719f63
softengrahmed <softengrahmed@gmail.com>
2025-04-07 21:02:23 -0400

    Update src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java

--- src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
+++ src/main/java/com/coveros/training/cartesianproduct/CartesianProduct.java
@@ -3,6 +3,7 @@ package com.coveros.training.cartesianproduct;
 import java.util.Set;
 
 public class CartesianProduct {
+    private CartesianProduct() {}
 
     public static <T> String calculate() {
         return "";
