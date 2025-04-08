a0a879837c278c96411cc42d9a059766e3510f36
softengrahmed <softengrahmed@gmail.com>
2025-04-07 21:57:52 -0400

    Update src/main/java/com/coveros/training/expenses/AlcoholCalculator.java

--- src/main/java/com/coveros/training/expenses/AlcoholCalculator.java
+++ src/main/java/com/coveros/training/expenses/AlcoholCalculator.java
@@ -1,6 +1,9 @@
 package com.coveros.training.expenses;
 
 public class AlcoholCalculator {
+    private AlcoholCalculator() {
+        // private constructor to hide the implicit public one
+    }
     public static AlcoholResult calculate(DinnerPrices dinnerPrices) {
         return AlcoholResult.returnEmpty();
     }
